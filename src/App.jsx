2a3bf66c52bf1030f0b136dbe0a8ded1d8d20b7b
Jogender Pal <ramanbalda5@gmail.com>
2024-07-22 17:57:35 +0530

    Added the UI navbar and navbar images changed to svg to keep the quality same

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
-import React, { lazy, Suspense } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { lazy, Suspense, useState } from "react";
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import { Analytics } from "@vercel/analytics/react";
-import BookNow from './Pages/BookNow';
+
+const BookNow = lazy(() => import('./Pages/BookNow'));
 const PrivateDinning = lazy(() => import("./Pages/PrivatedDinning"));
 const About = lazy(() => import("./Pages/About"));
 const Gift = lazy(() => import("./Pages/GiftVoucher"));
@@ -19,56 +20,50 @@ import { Caring } from "./Components/Caring";
 import { Sociable } from "./Components/Sociable";
 import Food from "./Pages/Food";
 import Drinks from "./Pages/Drinks";
-import { Footer } from "./Components/Footer";
 import Menu from "./Pages/Menu";
 import Venue from "./Pages/Venue";
-import Refund from "./Pages/Refund"; 
-import Modal from "./Components/Modal";
+import Refund from "./Pages/Refund";
 
 function App() {
+    const [modalContent, setModalContent] = useState(null);
+
+    const handleIconClick = (content) => {
+        setModalContent(content);
+    };
+
+    const closeModal = () => {
+        setModalContent(null);
+    };
+
     return (
-        <div className="relative font-customFont">
+        <div className="relative overflow-x-hidden">
             <BrowserRouter>
-                <InnerApp />
+                <Navbar onIconClick={handleIconClick} />
+                <Routes>
+                    <Route path="/" element={<Home modalContent={modalContent} closeModal={closeModal} />} />
+                    <Route path="/bookNow" element={<Suspense fallback={<h1>Loading...</h1>}><BookNow /></Suspense>} />
+                    <Route path="/menu" element={<Suspense fallback={<h1>Loading...</h1>}><Menu /></Suspense>} />
+                    <Route path="/privateDinning" element={<Suspense fallback={<h1>Loading...</h1>}><PrivateDinning /></Suspense>} />
+                    <Route path="/venue" element={<Suspense fallback={<h1>Loading...</h1>}><Venue /></Suspense>} />
+                    <Route path="/venue/restaurant" element={<Suspense fallback={<h1>Loading...</h1>}><Restaurant /></Suspense>} />
+                    <Route path="/venue/bar" element={<Suspense fallback={<h1>Loading...</h1>}><Bar /></Suspense>} />
+                    <Route path="/venue/cafe" element={<Suspense fallback={<h1>Loading...</h1>}><Cafe /></Suspense>} />
+                    <Route path="/venue/ird" element={<Suspense fallback={<h1>Loading...</h1>}><Ird /></Suspense>} />
+                    <Route path="/giftVoucher" element={<Suspense fallback={<h1>Loading...</h1>}><Gift /></Suspense>} />
+                    <Route path="/contact" element={<Suspense fallback={<h1>Loading...</h1>}><Contact /></Suspense>} />
+                    <Route path="/refund" element={<Suspense fallback={<h1>Loading...</h1>}><Refund /></Suspense>} />
+                    <Route path="/gallery" element={<Suspense fallback={<h1>Loading...</h1>}><Gallery /></Suspense>} />
+                    <Route path="/gallery/food" element={<Suspense fallback={<h1>Loading...</h1>}><Food /></Suspense>} />
+                    <Route path="/gallery/drinks" element={<Suspense fallback={<h1>Loading...</h1>}><Drinks /></Suspense>} />
+                    <Route path="/gallery/teams" element={<Suspense fallback={<h1>Loading...</h1>}><Team /></Suspense>} />
+                    <Route path="/about" element={<Suspense fallback={<h1>Loading...</h1>}><About /></Suspense>} />
+                    <Route path="/caring" element={<Suspense fallback={<h1>Loading...</h1>}><Caring /></Suspense>} />
+                    <Route path="/sociable" element={<Suspense fallback={<h1>Loading...</h1>}><Sociable /></Suspense>} />
+                </Routes>
+                <SpeedInsights />
+                <Analytics />
             </BrowserRouter>
         </div>
-    );
-}
-
-function InnerApp() {
-    const location = useLocation();
-    const shouldRenderNavbar = location.pathname !== "/";
-    const shouldRenderFooter = location.pathname !== "/";
-
-    return (
-        <>
-            {shouldRenderNavbar && <Navbar />}
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/bookNow" element={<BookNow />} />
-                <Route path="/modal" element={<Modal />} />
-                <Route path="/menu" element={<Suspense fallback={<h1>Loading...</h1>}><Menu /></Suspense>} />
-                <Route path="/privateDinning" element={<Suspense fallback={<h1>Loading...</h1>}><PrivateDinning /></Suspense>} />
-                <Route path="/venue" element={<Suspense fallback={<h1>Loading...</h1>}><Venue /></Suspense>} />
-                <Route path="/venue/restaurant" element={<Suspense fallback={<h1>Loading...</h1>}><Restaurant /></Suspense>} />
-                <Route path="/venue/bar" element={<Suspense fallback={<h1>Loading...</h1>}><Bar /></Suspense>} />
-                <Route path="/venue/cafe" element={<Suspense fallback={<h1>Loading...</h1>}><Cafe /></Suspense>} />
-                <Route path="/venue/ird" element={<Suspense fallback={<h1>Loading...</h1>}><Ird /></Suspense>} />
-                <Route path="/giftVoucher" element={<Suspense fallback={<h1>Loading...</h1>}><Gift /></Suspense>} />
-                <Route path="/contact" element={<Suspense fallback={<h1>Loading...</h1>}><Contact /></Suspense>} />
-                <Route path="/refund" element={<Suspense fallback={<h1>Loading...</h1>}><Refund /></Suspense>} />
-                <Route path="/gallery" element={<Suspense fallback={<h1>Loading...</h1>}><Gallery /></Suspense>} />
-                <Route path="/gallery/food" element={<Suspense fallback={<h1>Loading...</h1>}><Food /></Suspense>} />
-                <Route path="/gallery/drinks" element={<Suspense fallback={<h1>Loading...</h1>}><Drinks /></Suspense>} />
-                <Route path="/gallery/teams" element={<Suspense fallback={<h1>Loading...</h1>}><Team /></Suspense>} />
-                <Route path="/about" element={<Suspense fallback={<h1>Loading...</h1>}><About /></Suspense>} />
-                <Route path="/caring" element={<Suspense fallback={<h1>Loading...</h1>}><Caring /></Suspense>} />
-                <Route path="/sociable" element={<Suspense fallback={<h1>Loading...</h1>}><Sociable /></Suspense>} />
-            </Routes>
-            <SpeedInsights />
-            <Analytics />
-            {shouldRenderFooter && <Footer />}
-        </>
     );
 }
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,89 +1,27 @@
-import React, { useState, Suspense } from 'react';
-import { useNavigate } from 'react-router-dom';
-
+import { Suspense } from 'react';
 import logo from "/New/Group 85.png";
 import img from "/New/Home.png";
 import img1 from "/New/facebook.png";
 import img2 from "/New/instagram.png";
 import img3 from "/New/twitter.png";
-import home from "/New/house.png";
-import contact from "/New/contact.png";
-import giftvoucher from "/New/gift.png";
-import privatedining from "/New/hand-platter.png";
-import gallery from "/New/images.png";
-import about from "/New/info.png";
-import venue from "/New/map-pinned.png";
-import booknow from "/New/notebook-pen.png";
-import menu from "/New/notepad-text.png";
-
-const Modal = ({ activeModal, onClose }) => {
-    if (!activeModal) return null;
 
 
-    const renderModalContent = () => {
-        switch (activeModal) {
-            case 'booknow':
-                return (
-                    <div className="bg-customYellow p-4 rounded-lg w-full bg-opacity-50">
-                        <form className="flex flex-col space-y-4">
-                            <div className="flex flex-row space-x-3">
-                                <label className="text-customWhite">
-                                    Full Name:
-                                    <input type="text" name="name" className=" text-black w-32 rounded-lg" />
-                                </label>
-                                <label className="text-customWhite">
-                                    Contact:
-                                    <input type="tel" name="phone" className=" text-black w-32 rounded-lg" />
-                                </label>
-                            </div>
-                            <div className="flex flex-row space-x-8">
-                                <label className="text-customWhite">
-                                    Select Date:
-                                    <input type="date" name="date" className="border w-32 rounded-lg text-customBlack" />
-                                </label>
-                                <label className="text-customWhite">
-                                    Select Time:
-                                    <input type="time" name="time" className="border w-32 rounded-lg text-black" />
-                                </label>
-                            </div>
-                            <button className="text-customWhite text-xl w-[150px] p-2 mx-auto border">Find A Table</button>
-                        </form>
-                    </div>
-                );
-            default:
-                return <div>No content available.</div>;
-        }
-    };
 
-    return (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50" onClick={onClose}>
-            <div className="bg-transparent p-4 rounded-lg max-w-md mx-auto relative" onClick={(e) => e.stopPropagation()}>
-                {renderModalContent()}
-            </div>
-        </div>
-    );
-};
 
-export default function Home() {
-    const navigate = useNavigate();
-    const [activeModal, setActiveModal] = useState(null);
-    const [modalContentVisible, setModalContentVisible] = useState(false);
+
+
+
+
+export default function Home({ modalContent }) {
     
-    const openModal = (modalType) => setActiveModal(modalType);
-    const closeModal = () => {
-        setActiveModal(null);
-        setModalContentVisible(true);
-    };
 
-    const handleNavigate = (path) => {
-        closeModal();
-        navigate(path);
-    };
 
     return (
         <div className="flex bg-cover relative min-h-screen bg-fixed bg-opacity-100" style={{ backgroundImage: `url(${img})` }}>
-            <img src={logo} className="w-[239px] h-[69px] mt-4 ml-4" alt="Logo" />
-            <div className="flex flex-col items-center text-customWhite ml-44 mt-20">
+            <img src={logo} className="fixed w-[239px] h-[69px] mt-4 ml-4" alt="Logo" />
+            
+            
+            <div className="flex flex-col items-center text-customWhite mx-auto mt-20">
                 <h1 className="text-3xl tracking-widest">Welcome To </h1>
                 <h1 className="text-5xl tracking-widest mb-4">Fish N Dine</h1>
                 <p className="tracking-widest">Our Aim Is To Provide A Welcoming All-Day Experience</p>
@@ -93,22 +31,22 @@ export default function Home() {
                 <p className="tracking-widest text-xl">12Pm - 10Pm</p>
                 <p className="tracking-widest text-xl">Saturday & Sunday</p>
                 <p className="tracking-widest text-xl">2Pm - 12Am</p>
-                
-                <button className="tracking-widest text-xl border px-4 py-2 mt-4" onClick={() => openModal('booknow')}>Find A Table</button>
-                {activeModal && (<Modal onClose={closeModal} activeModal={activeModal}>
-                    {activeModal === 'booknow' && (
-                        <div>
-                           
-                        </div>
-                    )}
-                </Modal>
-                )}
-                {modalContentVisible && (
 
-                    <div className="mx-auto rounded-lg shadow-lg">
-                    
-                    </div>
-                )}
+                <button className="tracking-widest text-xl border px-4 py-2 mt-4" >Find A Table</button>
+                {modalContent && (
+                    <div className="flex flex-col items-center mx-auto ">
+                <p className="tracking-widest">Our Aim Is To Provide A Welcoming All-Day Experience</p>
+                <p className="tracking-widest">Rooted In Care For Our Customers, Staff, Produce And Land.</p>
+                <h1 className="tracking-widest text-3xl underline underline-offset-8 mt-4">Opening Time</h1>
+                <p className="tracking-widest text-xl mt-4">Monday To Friday</p>
+                <p className="tracking-widest text-xl">12Pm - 10Pm</p>
+                <p className="tracking-widest text-xl">Saturday & Sunday</p>
+                <p className="tracking-widest text-xl">2Pm - 12Am</p>
+                
+                <button className="tracking-widest text-xl border px-4 py-2 mt-4" >Find A Table</button>
+             
+              </div>
+              )}
                 <h1 className="tracking-widest text-3xl underline underline-offset-8 mt-4">Address</h1>
                 <p className="tracking-widest text-xl">Terminal Four Hounslow</p>
                 <p className="tracking-widest text-xl">TW6 3AF</p>
@@ -124,18 +62,7 @@ export default function Home() {
                 <img src={logo} className="w-[230px] h-[50px] mt-4 ml-4" alt="Powered By Logo" />
             </div>
 
-            <div className="relative w-[200px] h-[200px] mx-auto space-y-8">
-                <img src={home} onClick={() => handleNavigate('/')} className="absolute w-[29px] h-[29px] object-contain top-[60%] left-[50%] translate-x-[-50%] translate-y-[0%] cursor-pointer" alt="Home" />
-                <img src={giftvoucher} onClick={() => openModal('giftvoucher')} className="absolute w-[29px] h-[29px] object-contain top-[50%] left-[90%] translate-x-[20%] translate-y-[-30%] cursor-pointer" alt="Gift Voucher" />
-                <img src={privatedining} onClick={() => openModal('privatedining')} className="absolute w-[29px] h-[29px] object-contain top-[85%] left-[90%] translate-x-[-40%] translate-y-[-50%] cursor-pointer" alt="Private Dining" />
-                <img src={gallery} onClick={() => openModal('gallery')} className="absolute w-[29px] h-[29px] object-contain top-[90%] left-[50%] translate-x-[-50%] translate-y-[-50%] cursor-pointer" alt="Gallery" />
-                <img src={about} onClick={() => openModal('about')} className="absolute w-[29px] h-[29px] object-contain top-[85%] left-[15%] translate-x-[-50%] translate-y-[-50%] cursor-pointer" alt="About" />
-                <img src={contact} onClick={() => openModal('contact')} className="absolute w-[29px] h-[29px] object-contain top-[50%] right-[90%] translate-x-[-50%] translate-y-[-50%] cursor-pointer" alt="Contact" />
-                <img src={venue} onClick={() => openModal('venue')} className="absolute w-[29px] h-[29px] object-contain top-[14%] left-[15%] translate-x-[-50%] translate-y-[-50%] cursor-pointer" alt="Venue" />
-                <img src={booknow} onClick={() => openModal('booknow')} className="absolute w-[29px] h-[29px] object-contain top-[0%] left-[50%] translate-x-[-50%] translate-y-[0%] cursor-pointer" alt="Book Now" />
-                <img src={menu} onClick={() => openModal('menu')} className="absolute w-[29px] h-[29px] object-contain top-[14%] left-[85%] translate-x-[-50%] translate-y-[-50%] cursor-pointer" alt="Menu" />
-            </div>
-
+           
             <Suspense fallback={<h1>Loading...</h1>}>
                 {/* Suspended components can be added here */}
             </Suspense>
